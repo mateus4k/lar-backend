d1662144939eda1a4a8f395318fee50cdc5fdec7
Mateus <mateus4k@protonmail.ch>
2020-04-17 22:31:34 -0300

    Add Adonis "use" variable to globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,16 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: ["standard"],
+  extends: ['standard'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    use: true
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
-  rules: {},
-};
+  rules: {}
+}
