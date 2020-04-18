88052b238ff5732feddf3fd9d13e4d60bea1622a
Mateus <mateus4k@protonmail.ch>
2020-04-17 23:11:37 -0300

    Remove unused variables

--- test/functional/session.spec.js
+++ test/functional/session.spec.js
@@ -3,9 +3,6 @@ const { test, trait } = use('Test/Suite')('Session')
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const User = use('App/Models/User')
-
 trait('Test/ApiClient')
 
 test('it should return JWT token when session created', async ({
@@ -18,7 +15,7 @@ test('it should return JWT token when session created', async ({
     password: '123456'
   }
 
-  const user = await Factory.model('App/Models/User').create(sessionPayload)
+  await Factory.model('App/Models/User').create(sessionPayload)
 
   const response = await client.post('/sessions').send(sessionPayload).end()
 
