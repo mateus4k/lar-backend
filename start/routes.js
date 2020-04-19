48534d60ff7e5b5f6e6c19f458e2fd1114f227a2
Mateus <mateus4k@protonmail.ch>
2020-04-19 01:32:26 -0300

    Add forgot password

--- start/routes.js
+++ start/routes.js
@@ -1,25 +1,12 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
-
+/**
+ * Auth
+ */
 Route.post('/sessions', 'SessionController.store')
 Route.post('/register', 'RegisterController.store')
 Route.post('/forgot', 'ForgotPasswordController.store')
+Route.post('/reset', 'ResetPasswordController.store')
--- test/functional/forgotpassword.spec.js
+++ test/functional/forgotpassword.spec.js
@@ -1,6 +1,10 @@
 const { test, trait } = use('Test/Suite')('Forgot Password')
 
+const { subHours, format } = require('date-fns')
+
 const Mail = use('Mail')
+const Hash = use('Hash')
+const Database = use('Database')
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
@@ -14,26 +18,78 @@ test('it should send an email with reset password instructions', async ({
 }) => {
   Mail.fake()
 
-  const forgotPayload = {
-    email: 'mateus4k@protonmail.ch'
-  }
+  const email = 'mateus4k@protonmail.ch'
 
-  const user = await Factory.model('App/Models/User').create(forgotPayload)
+  const user = await Factory.model('App/Models/User').create({ email })
 
-  const response = await client.post('/forgot').send(forgotPayload).end()
-
-  response.assertStatus(204)
-
-  const recentEmail = Mail.pullRecent()
-
-  assert.equal(recentEmail.message.to[0].address, forgotPayload.email)
+  await client.post('/forgot').send({ email }).end()
 
   const token = await user.tokens().first()
 
+  const recentEmail = Mail.pullRecent()
+
+  assert.equal(recentEmail.message.to[0].address, email)
+
   assert.include(token.toJSON(), {
-    user_id: user.id,
     type: 'forgotpassword'
   })
 
   Mail.restore()
+})
+
+test('it should be able to reset password', async ({ assert, client }) => {
+  const email = 'mateus4k@protonmail.ch'
+
+  const user = await Factory.model('App/Models/User').create({ email })
+  const userToken = await Factory.model('App/Models/Token').make()
+
+  await user.tokens().save(userToken)
+
+  const response = await client
+    .post('/reset')
+    .send({
+      token: userToken.token,
+      password: '123456',
+      password_confirmation: '123456'
+    })
+    .end()
+
+  response.assertStatus(204)
+
+  await user.reload()
+
+  const checkPassword = await Hash.verify('123456', user.password)
+
+  assert.isTrue(checkPassword)
+})
+
+test('it cannot reset password after 2h of forgot password request', async ({
+  assert,
+  client
+}) => {
+  const email = 'mateus4k@protonmail.ch'
+
+  const user = await Factory.model('App/Models/User').create({ email })
+  const userToken = await Factory.model('App/Models/Token').make()
+
+  await user.tokens().save(userToken)
+
+  const dateWithSub = format(subHours(new Date(), 5), 'yyyy-MM-dd HH:ii:ss')
+
+  await Database.table('tokens')
+    .where('token', userToken.token)
+    .update('created_at', dateWithSub)
+
+  await userToken.reload()
+
+  const response = await client
+    .post('/reset')
+    .send({
+      token: userToken.token,
+      password: '123456',
+      password_confirmation: '123456'
+    })
+    .end()
+
+  response.assertStatus(400)
 })
