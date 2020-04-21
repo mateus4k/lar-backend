1d7eb1e0e70c3aa06faef882462baffed3a924cd
Mateus <mateus4k@protonmail.ch>
2020-04-21 18:24:48 -0300

    Cleanup controller

--- app/Controllers/Http/AcceptRuleController.js
+++ app/Controllers/Http/AcceptRuleController.js
@@ -1,18 +1,13 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 
-/**
- * Resourceful controller for interacting with acceptrules
- */
 class AcceptRuleController {
   /**
    * @param {object} ctx
-   * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response, auth }) {
+  async store({ response, auth }) {
     const user = await auth.getUser()
 
     user.accepted_terms = true
