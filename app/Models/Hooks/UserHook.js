0d6cab44681c3ee0536f4db1401649d76eba6bb7
Mateus <mateus4k@protonmail.ch>
2020-04-25 20:36:21 -0300

    Extract hash password hook to external hook file

---
+++ app/Models/Hooks/UserHook.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const Hash = use('Hash')
+
+const UserHook = (exports = module.exports = {})
+
+UserHook.hashPassword = async (userInstance) => {
+  if (userInstance.dirty.password) {
+    userInstance.password = await Hash.make(userInstance.password)
+  }
+}
