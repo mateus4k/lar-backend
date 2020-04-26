3caee6e829d9bddfcec5a6007f6f79f75735cf42
Mateus <mateus4k@protonmail.ch>
2020-04-25 22:27:46 -0300

    Clean unused code

--- test/functional/categories.spec.js
+++ test/functional/categories.spec.js
@@ -3,19 +3,12 @@ const { test, trait } = use('Test/Suite')('Family')
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const User = use('App/Models/User')
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Family = use('App/Models/Family')
-
 trait('Test/ApiClient')
 trait('DatabaseTransactions')
 trait('Auth/Client')
 
 test('standard categories should be created after creating a family', async ({
-  assert,
-  client
+  assert
 }) => {
   const family = await Factory.model('App/Models/Family').create()
 
